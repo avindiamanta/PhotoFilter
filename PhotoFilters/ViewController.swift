b79525aa1b302dbffaaed0d8fda6d25a648da458
Reid Weber <reid_weber@hotmail.com>
2014-10-13 09:45:53 -0700

    Initial Commit

--- PhotoFilters/ViewController.swift
+++ PhotoFilters/ViewController.swift
@@ -20,6 +20,20 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func photosButtonPressed(sender: AnyObject) {
+		
+		let alertController = UIAlertController(title: nil, message: "Choose an Option", preferredStyle: UIAlertControllerStyle.ActionSheet)
+		let galleryAction = UIAlertAction(title: "Gallery", style: UIAlertActionStyle.Default) { (action) -> Void in
+			self.performSegueWithIdentifier("SHOW_GALLERY", sender: self)
+		}
+		let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (action) -> Void in
+			// don't need the dismiss since the cancel style does it automatically
+			// self.dismissViewControllerAnimated(true, completion: nil)
+		}
+		alertController.addAction(galleryAction)
+		alertController.addAction(cancelAction)
+		self.presentViewController(alertController, animated: true, completion: nil)
+	}
 
 }
 
